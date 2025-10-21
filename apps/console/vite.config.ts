aff59fe4bdc4df3752ca24ad3b921ee872cbef80
Sanchit Rk <sanchitrrk@gmail.com>
2025-10-22 01:41:35 +0530

    wip

--- apps/console/vite.config.ts
+++ apps/console/vite.config.ts
@@ -3,13 +3,14 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
+import { cloudflare } from '@cloudflare/vite-plugin'
 
 const config = defineConfig({
   plugins: [
-    // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
+    cloudflare({ viteEnvironment: { name: 'ssr' } }),
     tailwindcss(),
     tanstackStart(),
     viteReact(),
@@ -17,9 +18,9 @@ const config = defineConfig({
   optimizeDeps: {
     exclude: ['@google-cloud/cloudbuild', '@google-cloud/storage'],
   },
-  ssr: {
-    external: ['@google-cloud/cloudbuild', '@google-cloud/storage'],
-  },
+  // ssr: {
+  //   external: ['@google-cloud/cloudbuild', '@google-cloud/storage'],
+  // },
 })
 
 export default config
